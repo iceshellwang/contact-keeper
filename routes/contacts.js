64289be2c0d825306f13121c718525fa44a666dd
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-02 23:32:44 +1000

    add CRUD method in contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,14 +2,22 @@ const express = require('express')
 
 const router = express.Router()
 
-// @route        GET api/auth
-// @desc         Get logged in user
+// @route        GET api/contacts
+// @desc         Get all users contacts
 // @ access      Private
-router.get('/', (req, res) => { res.send('Get logged in user') })
+router.get('/', (req, res) => { res.send('Get all contacts') })
 
-// @route        POST api/auth
-// @desc         Auth user and get token
+// @route        POST api/contacts
+// @desc         Add new contact
 // @ access      Public
-router.post('/', (req, res) => { res.send('Log in user') })
+router.post('/', (req, res) => { res.send('Add contact') })
+// @route        PUT api/contacts/:id
+// @desc         Update contact
+// @ access      Private
+router.put('/:id', (req, res) => { res.send('Update contact') })
+// @route        DELETE api/contacts/:id
+// @desc         Delete contact
+// @ access      Public
+router.delete('/:id', (req, res) => { res.send('Delete contact') })
 
 module.exports = router
