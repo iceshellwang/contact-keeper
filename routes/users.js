2af48d62f962e703c20a684ca87e3a82f5dae811
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-04 23:42:06 +1000

    wrap errors msg as array into response

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { body, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const router = express.Router()
 
 const User = require('../models/User')
@@ -10,6 +10,14 @@ router.post('/', [
     check('name', 'Plase add name').not().isEmpty(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
     check('email', 'Please include a valid email').isEmail()
-], (req, res) => { res.send(req.body) })
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    res.send('passed')
+
+
+})
 
 module.exports = router
