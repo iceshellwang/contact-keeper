1848cdef6182da89e85b22776dad7709b0d9db7a
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-04 23:32:53 +1000

    add check for name, email and pw

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 const express = require('express')
-
+const { body, validationResult } = require('express-validator');
 const router = express.Router()
 
+const User = require('../models/User')
 // @route        POST api/users
 // @desc         Register a user
 // @ access      Public
-router.post('/', (req, res) => { res.send('Register a user') })
+router.post('/', [
+    check('name', 'Plase add name').not().isEmpty(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+    check('email', 'Please include a valid email').isEmail()
+], (req, res) => { res.send(req.body) })
 
 module.exports = router
