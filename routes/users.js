486039272b08e56e2c78505ae4fa51d1f4c8a5bc
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-05 23:46:47 +1000

    save users to MongoDB

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { check, validationResult } = require('express-validator');
 const router = express.Router()
-
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 // @route        POST api/users
 // @desc         Register a user
@@ -10,14 +10,35 @@ router.post('/', [
     check('name', 'Plase add name').not().isEmpty(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
     check('email', 'Please include a valid email').isEmail()
-], (req, res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-    }
-    res.send('passed')
+],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
 
+        const { name, email, password } = req.body
+        try {
+            let user = await User.findOne({ email });
+            if (user) {
+                return res.status(400).json({ msg: 'User already exists' })
+            }
+            user = new User({
+                name, email, password
+            });
+            const salt = await bcrypt.genSalt(10)
 
-})
+            user.password = await bcrypt.hash(password, salt)
+
+            await user.save()
+
+            res.send('User saved')
+        }
+        catch (err) {
+            console.error(err.message)
+            res.status(500).send('Server Error')
+        }
+
+    })
 
 module.exports = router
