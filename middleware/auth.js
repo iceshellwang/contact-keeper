c1868ee5261ff076f70133125c77a851475c4da4
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-07 22:28:25 +1000

    assign userResult to request

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,4 +11,14 @@ module.exports = function (req, res, next) {
         return res.status(401).json({ msg: 'No token, authorization denied' })
     }
 
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'))
+
+        req.user = decoded.user
+        next()
+    }
+    catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' })
+    }
+
 }
