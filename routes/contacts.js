4d6bc8a903282459a96305961791cae9b51f25fc
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-08 23:54:11 +1000

    fix bug on async call

--- routes/contacts.js
+++ routes/contacts.js
@@ -24,7 +24,7 @@ router.get('/', auth, async (req, res) => {
 // @ access      Private
 router.post('/', [auth, [
     check('name', 'Name is required').not().isEmpty()
-]], (req, res) => {
+]], async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
