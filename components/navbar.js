be2a94404a0554445c700b47abd6564069f83d0b
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-13 23:51:50 +1000

    introduce navbar

---
+++ components/navbar.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+
+const Navbar = ({ title, icon }) => {
+    return (
+        <div></div>
+    )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string
+}
+
+Navbar.defaultProps = {
+    title: 'Contact Keeper',
+    icon: 'fas fa-id-card-alt'
+}
