8d187024acd5d7171e9d8ed10cd405fad7821c63
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-16 23:22:53 +1000

    introduce home page and about page in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
     <Router>
-      <div className="App">
+      <Fragment>
         <Navbar />
-      My APP
-    </div>
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </Fragment>
     </Router>
   );
 }
