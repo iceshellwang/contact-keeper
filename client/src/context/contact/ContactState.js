ac9795b3fd5b0148511b1aa1c26d11dd08e3c472
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-17 23:55:39 +1000

    introduce initialState for ContactState

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,2 +1,16 @@
 import React, { userReducer } from 'react'
 import uuid from 'uuid'
+import { ADD_CONTACT } from '../types'
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: 'jill Johnson',
+                email: 'jill'
+            }
+        ]
+
+    }
+}
