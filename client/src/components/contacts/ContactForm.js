de8722f8116d693f84e3b2eafde996984b61b16e
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-20 23:15:31 +1000

    add input boxes

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -10,7 +10,15 @@ const ContactForm = () => {
     const { name, email, phone, type } = contact
     return (
         <form>
-
+            <h2 className='text-primary'>
+                Add Contact
+</h2>
+            <input type='text' placeholder='Name' name='name' value={name} onChange={onChange}>
+            </input>
+            <input type='email' placeholder='Email' name='email' value={email} onChange={onChange}>
+            </input>
+            <input type='phone' placeholder='Phone' name='phone' value={phone} onChange={onChange}>
+            </input>
         </form>
     )
 }
