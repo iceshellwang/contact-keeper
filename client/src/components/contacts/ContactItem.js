f82e31f7acd4a9f6a7b7103f1099346daac492c6
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-25 23:29:47 +1000

    add setCurrent to edit btn

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import ContactContext from '../../context/contact/contactContext'
 const ContactItem = ({ contact }) => {
     const contactContext = useContext(ContactContext)
-    const { deleteContact } = contactContext
+    const { deleteContact, setCurrent } = contactContext
     const { id, name, email, phone, type } = contact
 
     const onDelete = () => {
@@ -25,7 +25,7 @@ const ContactItem = ({ contact }) => {
                 </li>)}
             </ul>
             <p>
-                <button className='btn btn-dark btn-sm'>Edit</button>
+                <button className='btn btn-dark btn-sm' onClick={() => setCurrent(contact)}>Edit</button>
                 <button className='btn btn-danger btn-sm' onClick={onDelete}>Delete</button>
             </p>
         </div>
