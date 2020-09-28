a07da24d208a21793cb7ff2964e53467fedc438e
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-28 23:47:54 +1000

    build onChange method for ContactFilter

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -4,6 +4,14 @@ import ContactContext from '../../context/contact/contactContext'
 const ContactFilter = () => {
     const contactContext = useContext(ContactContext)
     const text = useRef('')
+    const onChange = e => {
+        if (text.current.value !== '') {
+            contactContext.filterContacts(e.target.value)
+        }
+        else {
+            contactContext.clearFilter()
+        }
+    }
     return (
         <form>
             <input ref={text} type='text' placeholder='Filter Contacts...' onChange={onChange} />
