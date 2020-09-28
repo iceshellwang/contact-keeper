93703e5070ce1347d7a83186f74d9e4821c09768
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-28 23:42:04 +1000

    initialise ContactFilter

---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,12 @@
+import React, { useContext, useRef } from 'react'
+import ContactContext from '../../context/contact/contactContext'
+
+const ContactFilter = () => {
+    const contactContext = useContext(ContactContext)
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default ContactFilter
