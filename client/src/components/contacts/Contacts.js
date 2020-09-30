166e9ae24876a424e21c42d4814fc8ac88a17c56
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-09-30 23:42:03 +1000

    introduce transitionGroup into contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useContext } from 'react'
 import ContactContext from '../../context/contact/contactContext'
 import ContactItem from './ContactItem'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 export const Contacts = () => {
     const contactContext = useContext(ContactContext)
     const { contacts, filtered } = contactContext
@@ -9,11 +10,14 @@ export const Contacts = () => {
     }
     return (
         <Fragment>
-            {filtered !== null ? filtered.map(contact => <ContactItem key={contact.id} contact={contact} />
+            <TransitionGroup>
 
-            ) : contacts.map(contact =>
-                <ContactItem key={contact.id} contact={contact} />)}
+                {filtered !== null ? filtered.map(contact => <ContactItem key={contact.id} contact={contact} />
 
+                ) : contacts.map(contact =>
+                    <ContactItem key={contact.id} contact={contact} />)}
+
+            </TransitionGroup>
         </Fragment>
     )
 }
