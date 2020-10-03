cb690505d36dfbd3a5dc774e1d23841e57805992
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-10-03 23:30:42 +1000

    declare initial state for AuthState

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -14,61 +14,19 @@ import {
 
 const AuthState = props => {
     const initialState = {
-        contacts: [
-            {
-                id: 1,
-                name: 'Jill Johnson',
-                email: 'jill@gmail.com',
-                phone: '111-111-1111',
-                type: 'personal'
-            },
-            {
-                id: 2,
-                name: 'Sara Watson',
-                email: 'sara@gmail.com',
-                phone: '222-222-2222',
-                type: 'personal'
-            },
-            {
-                id: 3,
-                name: 'Harry White',
-                email: 'harry@gmail.com',
-                phone: '333-333-3333',
-                type: 'professional'
-            }
-        ],
-        current: null,
-        filtered: null
+        token: localStorage.getItem('token'),
+        isAuthenticated: null,
+        loading: true,
+        error: null
+
 
     }
 
-    const [state, dispatch] = useReducer(contactReducer, initialState)
+    const [state, dispatch] = useReducer(authReducer, initialState)
 
     //ADD CONTACT
 
-    const addContact = contact => {
-        contact.id = uuid
-        dispatch({ type: ADD_CONTACT, payload: contact })
-    }
 
-    //DELETE CONTACT
-
-    const deleteContact = id => {
-
-        dispatch({ type: DELETE_CONTACT, payload: id })
-    }
-
-    //SET CURRENT CONTACT
-    const setCurrent = contact => {
-
-        dispatch({ type: SET_CURRENT, payload: contact })
-    }
-    //CLEAR CURRENT CONTACT
-
-    const clearCurrent = () => {
-
-        dispatch({ type: CLEAR_CURRENT })
-    }
 
     //UPDATE CONTACT
     const updateContact = contact => {
