619beb99f36dc62299c72d594a7a8c8ee0a89f57
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-10-03 23:56:48 +1000

    add detailed form field

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -9,15 +9,30 @@ export const Register = () => {
     })
     const { name, email, password, password2 } = user
     return (
-        <div>
+        <div className='form-container'>
             <h1>
                 Account
-    <span>
+    <span className='text-primary'>
                     Register
     </span>
             </h1>
             <form>
-
+                <div className='form-group'>
+                    <label htmlFor='name'>Name</label>
+                    <input type='text' name='name' value={name} onChange={onChange}></input>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='email'>Email Address</label>
+                    <input type='email' name='email' value={email} onChange={onChange}></input>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' name='password' value={password} onChange={onChange}></input>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='password2'>Password2</label>
+                    <input type='password2' name='password2' value={password2} onChange={onChange}></input>
+                </div>
             </form>
         </div>
     )
