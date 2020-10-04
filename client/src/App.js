b18479914549cef948ee6fea37c40b468f051f7c
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-10-04 23:45:42 +1100

    add login route

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import AuthState from './context/auth/AuthState'
 import ContactState from './context/contact/ContactState'
@@ -19,6 +20,7 @@ function App() {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
               </Switch>
             </div>
           </Fragment>
