3aa66bf4ac434380d62718f74cf4c757dc80d480
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-10-10 23:52:38 +1100

    check error content in useEffect

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,16 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import AlertContext from '../../context/alert/alertContext'
 import AuthContext from '../../context/auth/authContext'
 const Register = () => {
     const alertContext = useContext(AlertContext)
     const authContext = useContext(AuthContext)
     const { setAlert } = alertContext
-    const { register } = authContext
+    const { register, error } = authContext
+    useEffect(() => {
+        if (error === 'User already exists') {
+            setAlert(error, 'danger')
+        }
+    }, [error])
     const [user, setUser] = useState({
         name: '',
         email: '',
