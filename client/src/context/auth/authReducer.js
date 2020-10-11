6678a708bdf67a81187f4536bb2ac16309ffe8a9
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-10-11 20:13:16 +1100

    add loadUser and authError cases

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -5,12 +5,19 @@ import {
     CLEAR_CURRENT,
     UPDATE_CONTACT,
     FILTER_CONTACTS,
-    CLEAR_FILTER, REGISTER_SUCCESS, REGISTER_FAIL, CLEAR_ERRORS
+    CLEAR_FILTER, REGISTER_SUCCESS, REGISTER_FAIL, CLEAR_ERRORS, USER_LOADED, AUTH_ERROR
 }
     from '../types'
 
 export default (state, action) => {
     switch (action.type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: action.payload
+            }
         case REGISTER_SUCCESS:
             localStorage.setItem('token', action.payload.token)
             return {
@@ -20,6 +27,7 @@ export default (state, action) => {
                 loading: false
             }
         case REGISTER_FAIL:
+        case AUTH_ERROR:
             localStorage.removeItem('token')
             return {
                 ...state,
@@ -34,6 +42,7 @@ export default (state, action) => {
                 ...state,
                 error: null
             }
+
         default:
             return state;
     }
