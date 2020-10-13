a646693d31d963fb0b36158b9f80f91209dae3c5
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-10-13 23:48:16 +1100

    introduce context to login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,24 @@
 import React, { useState } from 'react'
-
+import AlertContext from '../../context/alert/alertContext'
+import AuthContext from '../../context/auth/authContext'
 const Login = () => {
+    const alertContext = useContext(AlertContext)
+    const authContext = useContext(AuthContext)
+    const { setAlert } = alertContext
+    const { login, error, clearErrors, isAuthenticated } = authContext
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            props.history.push('/')
+        }
+        if (error === 'Invalid Credentials') {
+            setAlert(error, 'danger')
+            clearErrors()
+        }
+        //eslint-disable-next-line
+    }, [error, isAuthenticated, props.history])
+
+
     const [user, setUser] = useState({
         email: '',
         password: '',
@@ -12,7 +30,7 @@ const Login = () => {
     }
     const onSubmit = e => {
         e.preventDefault()
-        console.log('Login submit')
+
     }
     return (
         <div className='form-container'>
