465569757422d04ce434feaafd6892b5454c4e6b
iceshellwang <35946230+iceshellwang@users.noreply.github.com>
2020-10-16 23:43:10 +1100

    refactor addContact action

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react'
 import { v4 as uuid } from "uuid";
+import axios from 'axios'
 import ContactContext from './contactContext'
 import contactReducer from './contactReducer'
 import {
@@ -9,33 +10,12 @@ import {
     CLEAR_CURRENT,
     UPDATE_CONTACT,
     FILTER_CONTACTS,
-    CLEAR_FILTER
+    CLEAR_FILTER, CONTACT_ERRORS
 } from '../types'
 
 const ContactState = props => {
     const initialState = {
         contacts: [
-            {
-                id: 1,
-                name: 'Jill Johnson',
-                email: 'jill@gmail.com',
-                phone: '111-111-1111',
-                type: 'personal'
-            },
-            {
-                id: 2,
-                name: 'Sara Watson',
-                email: 'sara@gmail.com',
-                phone: '222-222-2222',
-                type: 'personal'
-            },
-            {
-                id: 3,
-                name: 'Harry White',
-                email: 'harry@gmail.com',
-                phone: '333-333-3333',
-                type: 'professional'
-            }
         ],
         current: null,
         filtered: null
@@ -46,9 +26,20 @@ const ContactState = props => {
 
     //ADD CONTACT
 
-    const addContact = contact => {
-        contact.id = uuid
-        dispatch({ type: ADD_CONTACT, payload: contact })
+    const addContact = async contact => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        try {
+            const res = await axios.post('/api/contacts', contact, config)
+            dispatch({ type: ADD_CONTACT, payload: res.data })
+        }
+        catch (err) {
+            dispatch({ type: CONTACT_ERRORS, payload: err.response.msg })
+        }
+
     }
 
     //DELETE CONTACT
